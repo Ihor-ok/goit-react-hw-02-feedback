375f26d91380e4bc54defc8eece16040e3eeb1a1
Ihor <kovaluk_igor@ukr.net>
2024-01-27 20:28:42 +0100

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,59 @@
+import { Component } from 'react'
+import Section from 'components/Section/Section.jsx'
 
+class App extends Component {
+  
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+}
+  
+  goodOnClick = (evt) => {
+    this.setState((prevState) => {return {good: prevState.good + 1}})
+  }
 
-export default function App() {
-  return (
-    <>
-     
-    </>
-  )
+  neutralOnClick = (evt) => {
+    this.setState((prevState) => {return {neutral: prevState.neutral + 1}})
+  }
+
+  badOnClick = (evt) => {
+     this.setState((prevState) => {return {bad: prevState.bad + 1}})
+  }
+
+  countTotalFeedback = ( good, neutral, bad ) => {
+    return bad + neutral + good
+
+  }
+
+  countPositiveFeedbackPercentage = (good, neutral, bad) => {
+    let positiveFeedbackPercentage = good/(good + neutral + bad)*100
+        
+    if (positiveFeedbackPercentage >= 0) {
+      return Math.round(positiveFeedbackPercentage)
+    }
+    
+    return 0
+  }
+  
+  render() {
+    return (
+      <>
+        <Section
+        good={this.state.good}
+        neutral={this.state.neutral}
+        bad={this.state.bad}
+        goodOnClick={this.goodOnClick}
+        neutralOnClick={this.neutralOnClick}
+        badOnClick={this.badOnClick}
+        countTotalFeedback={this.countTotalFeedback}
+        countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage}/>
+      </>)
+
+  }
+ 
+  
 };
+
+
+export default App
